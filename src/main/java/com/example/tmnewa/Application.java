42cd3c1edb75e449e1838ff1ce55d97634de8037
ghost751222 <ghost751222@gmail.com>
2025-01-14 16:02:26 +0800

    too many

--- src/main/java/com/example/tmnewa/Application.java
+++ src/main/java/com/example/tmnewa/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @PropertySource(value = "classpath:persistence.properties")
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
