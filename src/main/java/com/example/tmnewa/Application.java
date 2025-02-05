476ee90107e00cd16fd071df48763af0f62c87ce
ghost751222 <ghost751222@gmail.com>
2025-02-05 13:26:32 +0800

    add mysql persistence-mysql.properties

--- src/main/java/com/example/tmnewa/Application.java
+++ src/main/java/com/example/tmnewa/Application.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
-@PropertySource(value = "classpath:persistence.properties")
+@PropertySource(value = "classpath:persistence-${spring.database.type}.properties")
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Application extends SpringBootServletInitializer {
 
